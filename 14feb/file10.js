c25e518d7fe81a3985da1f22d410cdf9b4bb5086
Jashwanth E M <jashwanththestar@gmail.com>
2025-02-14 13:06:09 +0530

    JavaScript  14th feb

--- 14feb/file10.js
+++ 14feb/file10.js
@@ -1,10 +1,10 @@
-let mark = 100
+let mark = 101
 
 if (mark === 100) {
     console.log("Phenomenal");
 }
 
-else if (mark > 85) {
+else if (mark > 85 && mark < 100) {
     console.log("Excellent");
 }
 else if (mark >= 71 && mark <= 85) {
@@ -14,6 +14,9 @@ else if (mark >= 71 && mark <= 85) {
 else if (mark >= 51 && mark <= 70) {
     console.log("Practice Well");
 }
-else {
+else if (mark <= 50 && mark >= 0) {
     console.log("HardWork is needed");
+}
+else {
+    console.log("Invalid Mark");
 }
